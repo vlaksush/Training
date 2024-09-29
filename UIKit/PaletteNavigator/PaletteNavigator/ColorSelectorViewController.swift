bea2fc4e4eb471af05ca483f2f627b4325d912fa
Ravi Shankar <ravishankar@outlook.in>
2024-09-29 19:39:46 +0530

    PaletteNavigator
    Navigation Controller and TableView Demo

---
+++ UIKit/PaletteNavigator/PaletteNavigator/ColorSelectorViewController.swift
@@ -0,0 +1,15 @@
+//
+//  ViewController.swift
+//  PaletteNavigator
+//
+//
+
+import UIKit
+
+class ColorSelectorViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
+
---
+++ UIKit/PaletteNavigator/PaletteNavigator/MyColors.swift
@@ -0,0 +1,34 @@
+//
+//  MyColors.swift
+//  PaletteNavigator
+//
+//  Created by Ravi Shankar on 29/09/24.
+//
+
+import UIKit
+
+enum MyColors: String, CaseIterable {
+    case blue, cyan, green, orange, red, yellow, purple, gray
+
+    var displayName: String {
+        rawValue.capitalized
+    }
+    
+    var uiColor: UIColor {
+        switch self {
+        case .blue: return .blue
+        case .cyan: return .cyan
+        case .green: return .green
+        case .orange: return .orange
+        case .red: return .red
+        case .yellow: return .yellow
+        case .purple: return .purple
+        case .gray: return .gray
+        }
+    }
+
+    static func colour(at index: Int) -> MyColors? {
+        guard index >= 0, index < allCases.count else { return nil }
+        return allCases[index]
+    }
+}
